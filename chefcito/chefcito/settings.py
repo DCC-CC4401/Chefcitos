7c20736379d6409d76e9691cdac8cd758675ce19
Roberto <robertoarayaday@gmail.com>
2020-10-13 20:57:24 -0300

    Mejorar modelo Receta

--- chefcito/chefcito/settings.py
+++ chefcito/chefcito/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import chefcitoapp
+import receta
+import os
 
 AUTH_USER_MODEL = 'chefcitoapp.User'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chefcitoapp',
+    'receta',
     ]
 
 MIDDLEWARE = [
--- chefcito/receta/models.py
+++ chefcito/receta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from chefcito.chefcitoapp.models import User
 
 
 
@@ -18,18 +19,24 @@ class Ingrediente(models.Model):
 
 
 class Receta(models.Model):
-    nombre_usuario = models.ForeignKey(Usuario, default="general", on_delete=models.CASCADE)
+    nombre_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     receta_nombre= models.CharField(primary_key=True, max_length=250)
 
     preparacion=models.TextField(max_length=1300)
     duracion = models.TimeField()
-    receta_foto=models.TextField(max_length=200)
+    receta_foto=models.ImageField(upload_to='fotos_recetas')
     fecha_publ=models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     descripcion=models.TextField()
     ingrediente=models.ManyToManyField(Ingrediente)
 
     def __str__(self):
         return self.receta_nombre
+
+
+class CalificaReceta(models.Model):
+    nombre_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    receta_id = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    nota=models.DecimalField({'min_value': 0, 'max_value':10.0})
 
 # Create your models here.
 
