ddf307732bb697909b4002149b31b92114f174af
galvadiz <geraldine.alvadiz@ug.uchile.cl>
2020-10-27 09:15:32 -0300

    Agregados los modelos Receta e Ingredientes a Admin

--- chefcito/chefcitoapp/admin.py
+++ chefcito/chefcitoapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Receta, Ingrediente
 
 admin.site.register(User)
+admin.site.register(Receta)
+admin.site.register(Ingrediente)
