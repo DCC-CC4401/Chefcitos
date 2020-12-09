83660db4c3961341b2bf309d62e7090253bb71c4
GenesisMoraga <genesis.moraga@ing.uchile.cl>
2020-12-09 19:47:23 -0300

    models actualizado

--- chefcito/chefcitoapp/models.py
+++ chefcito/chefcitoapp/models.py
@@ -48,11 +48,21 @@ class Receta(models.Model):
     receta_foto=models.ImageField(default=False, upload_to='fotos_recetas/', blank=True, null=True)
     fecha_publ=models.DateTimeField(auto_now_add=True)
     descripcion=models.TextField()
-    ingrediente=models.ManyToManyField(Ingrediente)
+    
+    vegano=models.BooleanField(default=False)
+    vegetariano=models.BooleanField(default=False)
+    diabetico=models.BooleanField(default=False)
+    celiaco=models.BooleanField(default=False)
+    int_lactosa=models.BooleanField(default=False)
 
     def __str__(self):
         return self.receta_nombre
 
+class RecetaIngrediente(models.Model):
+    receta_id=models.ForeignKey(Receta, on_delete=models.CASCADE)
+    ingrediente_id=models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+    unidad=models.BigIntegerField()
+    medida=models.TextField()
 
 class CalificaReceta(models.Model):
     nombre_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
